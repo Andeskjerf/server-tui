69e246b84e9e97027444119e5b1ec6bb91600d98
Andeskjerf <predgd@pm.me>
2024-12-30 12:15:58 +0100

    remove on done + move msg processing

--- src/services/socket.rs
+++ src/services/socket.rs
@@ -29,6 +29,25 @@ impl SocketService {
         UnixListener::bind(&bind_path).expect("unable to initialize socket listener")
     }
 
+    async fn process_message(buffer: Vec<u8>, queue: Arc<Mutex<HashMap<String, String>>>) {
+        if let Ok(string) = String::from_utf8(buffer) {
+            let msg: SocketMessage = serde_json::from_str(
+                // remove any additional zeros from the buffer
+                string.trim_end_matches(char::from(0)),
+            )
+            .unwrap();
+
+            let mut lock = queue.lock().await;
+
+            if msg.status.to_lowercase() != "done" {
+                (*lock).insert(msg.title, msg.status);
+                return;
+            }
+
+            (*lock).remove(&msg.title);
+        }
+    }
+
     // TODO: needs to handle errors / bad input / kick out clients
     // FIXME: very nested
     pub async fn run(&self) {
@@ -41,17 +60,13 @@ impl SocketService {
                     let queue = Arc::clone(&queue);
                     tokio::spawn(async move {
                         match stream.read(&mut buffer).await {
-                            Ok(_) => {
-                                if let Ok(string) = String::from_utf8(buffer) {
-                                    let msg: SocketMessage = serde_json::from_str(
-                                        // remove any additional zeros from the buffer
-                                        string.trim_end_matches(char::from(0)),
-                                    )
-                                    .unwrap();
-                                    queue.lock().await.insert(msg.title, msg.status);
-                                }
+                            Err(err) => {
+                                println!("err: {err}");
                             }
-                            Err(err) => println!("err: {err}"),
+                            Ok(_) => {
+                                // static function since dealing with borrowed self is hard
+                                SocketService::process_message(buffer, queue).await;
+                            }
                         }
                     });
                 }
