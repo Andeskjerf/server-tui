fc983167b90240904688dbaa62d3ac8de544f554
Andeskjerf <predgd@pm.me>
2025-01-01 12:23:20 +0100

    init in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{io, sync::Arc};
 
 use app::App;
-use services::socket::SocketService;
+use services::{process_watcher::ProcessWatcher, socket::SocketService};
 
 mod api;
 mod app;
@@ -13,11 +13,16 @@ mod widgets;
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let socket = SocketService::new("server-tui.socket");
-    let queue = Arc::clone(&socket.queue);
+    let process_watcher = ProcessWatcher::new();
+    let socket_messages = Arc::clone(&socket.queue);
+    let process_updates = Arc::clone(&process_watcher.status);
     socket.run().await;
 
+    process_watcher.cleanup_task();
+    process_watcher.watch_process("steam".to_string());
+
     let terminal = ratatui::init();
-    let mut app = App::new(terminal, queue)?;
+    let mut app = App::new(terminal, socket_messages, process_updates)?;
     let result = app.run().await;
 
     ratatui::restore();
