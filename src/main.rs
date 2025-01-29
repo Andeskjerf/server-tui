612c3937b8bd046960f3c19117cf897bcc0583ca
Andeskjerf <predgd@pm.me>
2025-01-29 17:39:01 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::{env, io, sync::{Arc, Mutex}};
+use std::{
+    env, io,
+    sync::{Arc, Mutex},
+};
 
 use crate::traits::runnable::Runnable;
 use app::App;
 use services::{event_bus::EventBus, process_watcher::ProcessWatcher, socket::SocketService};
-// use tokio::sync::Mutex;
 
 mod api;
 mod app;
@@ -17,12 +19,10 @@ mod widgets;
 async fn main() -> io::Result<()> {
     let event_bus = Arc::new(Mutex::new(EventBus::new()));
 
+    let to_watch = env::args().skip(1).collect::<Vec<String>>();
     let services: Vec<Box<dyn Runnable>> = vec![
         Box::new(SocketService::new(Arc::clone(&event_bus), "server-tui.sock").await),
-        Box::new(ProcessWatcher::new(
-            Arc::clone(&event_bus),
-            env::args().skip(1).collect::<Vec<String>>(),
-        )),
+        Box::new(ProcessWatcher::new(Arc::clone(&event_bus), to_watch)),
     ];
 
     for s in services {
